c120b0d5482d040983c2ee4ecdede00a2f798c1d
330040431 <330040431@qq.com>
2019-12-18 15:32:33 +0800

    init commit

---
+++ MySlot/MySlot.lua
@@ -0,0 +1,291 @@
+﻿local MySlot_Profile={}
+local MacroIcon = {}
+
+for i=1, GetNumMacroIcons() do
+	MacroIcon[(GetMacroIconInfo(i))] = i
+end
+
+local map_String={
+	["spell"]="S",
+	["macro"]="M",
+	["item"]="I",
+	["companion"]="C",
+}
+
+local function getSpellString(arg1,arg0)
+	if arg1==0 then
+		return ""
+	end
+	local a,b=GetSpellName(arg1,BOOKTYPE_SPELL)
+	if b~="" then
+		return a.."("..b..")"
+	else
+		return a
+	end	
+end
+
+local companionitem=[[{["b"]="%s",["c"]=%d}]]
+local function getCompanionString(arg0,arg1)
+	return string.format(companionitem, arg0, arg1)
+end
+
+local function myPickupCompanion(arg1)
+	if type(arg1)=="table" then
+		PickupCompanion(arg1["b"], arg1["c"])
+	end
+end
+
+--local macroitem=[[{["id"]=%s,["name"]="%s",["icon"]=%d,["body"]=%s,["perchar"]=%s,}]]
+local macroitem=[[{["b"]="%s",["c"]=%d,["d"]=%s,["e"]=%s,}]]
+
+local function getMacroString(arg1)
+	local name, iconTexture, body, perchar,l = GetMacroInfo(arg1)
+	if name and body then
+		
+		local iconx=MacroIcon[iconTexture] or 1
+		if string.find(body,"#show")==1 then
+			iconx=1
+		end
+		return string.format(macroitem,name, iconx,"[[".. body.."]]", tonumber(arg1)>36 and "1" or "nil")
+	end
+	return arg1
+end
+
+local function findMacro(arg1)
+	for i=1,54 do
+		local name, iconTexture, body, perchar = GetMacroInfo(i)
+		local icon=MacroIcon[iconTexture] or 1
+		if body==(arg1["d"]) then
+			
+			return EditMacro(i,arg1["b"],arg1["c"],(arg1["d"]),arg1["e"])
+		end
+	end
+	
+	return nil
+end
+
+local function myCreateMacro(arg1)
+	local g,l = GetNumMacros()
+	if ((arg1["e"] and l==18) or (arg1["e"]==nil and g==36)) then
+		MySlot_Print("宏 ["..arg1["b"].." ] 被忽略，因为你没有在公共或个人宏区域没有足够的空格，请准备足够的空格后重新导入")
+		return nil
+	end
+	return CreateMacro(arg1["b"],arg1["c"],(arg1["d"]),arg1["e"],1)
+end
+
+local function myGetMacroId(arg1)
+	if type(arg1)=="table" then
+		return findMacro(arg1) or myCreateMacro(arg1)
+	end
+	return arg1
+end
+
+
+local function myGetActionInfo(i)
+	local arg0,arg1,arg2,arg3 = GetActionInfo(i)
+	
+	if map_String[arg0 or "x"] then
+	
+		if(arg0=="spell")then
+			return "\"S\"","\""..getSpellString(arg1,arg0).."\""
+		end	
+
+		if(arg0=="macro")then
+			return "\"M\"",getMacroString(arg1)
+		end	
+		
+		if(arg0=="companion")then
+			return "\"C\"",getCompanionString(arg2,arg1)
+		end	
+
+		return "\"I\"","\""..arg1.."\""
+		
+	end
+	
+	return nil
+end
+
+local function isChange(i)
+	local arg0,arg1 = myGetActionInfo(i)
+	
+	if MySlot_Profile[i]==nil then
+		return arg0~=nil
+	end
+	
+	if MySlot_Profile[i]["a"]~=arg0 then
+		return 1
+	end 
+
+	if MySlot_Profile[i]["b"]~=arg1 then
+		return 1
+	end 
+	
+	--[[
+	由于 宏 指针 变化不确定 认为所有宏都发生了变化
+	if arg0=="macro" then
+		return not findMacro(arg1)
+	end]]
+	
+	return nil
+end
+
+local keyitem=[[[%s]=%s,]]
+
+local function myGetKeyBinding()
+	local t={}
+	for i=1,GetNumBindings() do
+		command, key1, key2=GetBinding(i)	
+		if key1=="\\" then
+			key1="\\\\"
+		end
+		
+		if key2=="\\" then
+			key2="\\\\"
+		end
+		
+		
+		if key1 then
+			t[#t+1]=string.format(keyitem,"\""..key1.."\"","\""..command.."\"")
+		end
+		
+		if key2 then
+			t[#t+1]=string.format(keyitem,"\""..key2.."\"","\""..command.."\"")
+		end
+		
+	end
+	
+	return table.concat(t)
+end
+
+StaticPopupDialogs["MYSLOT_MSGBOX"] = {
+	text = "你 确定 要导入么？？？",
+	button1 = "确定",
+	button2 = "取消",
+	timeout = 0,
+	whileDead = 1,
+	hideOnEscape = 1,
+	multiple = 1,
+	OnAccept=function()
+		local i,j,iv,jv
+		local changed={}
+		local add=nil
+	
+		for i=1,120 do
+			if isChange(i) then
+				tinsert(changed,i)	
+			end				
+		end
+		
+		for i,iv in pairs(changed) do
+			if MySlot_Profile[iv]==nil then
+					PickupAction(iv)
+					ClearCursor()						
+			elseif MySlot_Profile[iv].a=="S" then
+				PickupSpell(MySlot_Profile[iv].b);
+			elseif MySlot_Profile[iv].a=="I" then
+				PickupItem(MySlot_Profile[iv].b)		
+			elseif MySlot_Profile[iv].a=="M" then
+				PickupMacro(myGetMacroId(MySlot_Profile[iv].b))
+			elseif MySlot_Profile[iv].a=="C" then
+				myPickupCompanion(MySlot_Profile[iv].b)
+			elseif MySlot_Profile[iv].a=="nil" then
+					PickupAction(iv)
+					ClearCursor()
+			elseif MySlot_Profile[iv].a==nil then
+					PickupAction(iv)
+					ClearCursor()					
+			end
+			
+			PlaceAction(iv)	
+			ClearCursor()
+	
+		end		
+		
+		if MySlot_Profile[999] then
+			for i,v in pairs(MySlot_Profile[999]) do
+				SetBinding(i,v)
+			end
+		end
+		
+		MySlot_Print("所有按钮及按键邦定位置恢复完毕")
+	end,
+}
+
+
+local slotitem=[[[%d]={["a"]=%s,["b"]=%s,},]]
+
+function MySlot_Export()
+	local s=""
+	local i
+	local t={}
+	for i=1,120 do
+		local arg0,arg1 = myGetActionInfo(i)
+			if arg0 then
+				t[#t+1]=string.format(slotitem,i,arg0,arg1)
+			end
+	end	
+	
+	t[#t+1]="[999]={"..myGetKeyBinding().."},"
+	
+	s="@ --------------------\n"..s
+	s="@ 问题/建议请联系 farmer1992@gmail.com\n"..s
+	s="@ \n"..s
+	s="@ 等级："..UnitLevel("player").."\n"..s
+	s="@ 职业："..UnitClass("player").."\n"..s
+	s="@ 人物："..UnitName("player").."\n"..s
+	s="@ 天赋："..select(3,GetTalentTabInfo(1)).."/"..select(3,GetTalentTabInfo(2)).."/"..select(3,GetTalentTabInfo(3)).."\n"..s
+	s="@ Myslot 导出数据"..date().."\n"..s
+	
+	s=s..HumBase64.enc(table.concat(t))
+
+	--s=s..(table.concat(t))
+
+	MYSLOT_ReportFrame_EditBox:SetText(s)
+	MYSLOT_ReportFrame_EditBox:HighlightText()
+
+end
+
+function MySlot_Import()
+
+	if InCombatLockdown() then
+		MySlot_Print("请在非战斗时候使用导入功能")
+	end
+
+	local s=MYSLOT_ReportFrame_EditBox:GetText() or ""
+	s=string.gsub(s,"(@.[^\n]*\n)","")
+	s=string.gsub(s,"\n","")
+	s=string.gsub(s,"\r","")
+	s=HumBase64.dec(s)
+	
+	--MYSLOT_ReportFrame_EditBox:SetText(s)
+	--MYSLOT_ReportFrame_EditBox:HighlightText()	
+	
+	local f=loadstring("return {"..s.."}")
+	if f and s~="" then
+		MySlot_Profile=f()
+
+		StaticPopup_Show("MYSLOT_MSGBOX")
+	else
+		MySlot_Print("导入字符串有错误 不能导入")
+	end
+	
+end
+
+
+function MySlot_Print(msg)
+	DEFAULT_CHAT_FRAME:AddMessage("|CFFFF0000<|r|CFFFFD100My Slot|r|CFFFF0000>|r"..(msg or ""))
+end
+
+SlashCmdList["Myslot"] = function()
+	MYSLOT_ReportFrame:Show()
+end
+SLASH_Myslot1 = "/Myslot"
+
+
+
+function MySlot_Clearall()
+		for i=1,120 do
+				PickupAction(i)
+				ClearCursor()
+		end
+end
---
+++ MySlot/humbase64.lua
@@ -0,0 +1,117 @@
+﻿--[[
+
+	haffman compressed spical base64 by T.G.(farmer1992@gmail.com)
+	QQ 22267156
+]]
+
+
+local base64chars = {[0]='A',[1]='B',[2]='C',[3]='D',[4]='E',[5]='F',[6]='G',[7]='H',[8]='I',[9]='J',[10]='K',[11]='L',[12]='M',[13]='N',[14]='O',[15]='P',[16]='Q',[17]='R',[18]='S',[19]='T',[20]='U',[21]='V',[22]='W',[23]='X',[24]='Y',[25]='Z',[26]='a',[27]='b',[28]='c',[29]='d',[30]='e',[31]='f',[32]='g',[33]='h',[34]='i',[35]='j',[36]='k',[37]='l',[38]='m',[39]='n',[40]='o',[41]='p',[42]='q',[43]='r',[44]='s',[45]='t',[46]='u',[47]='v',[48]='w',[49]='x',[50]='y',[51]='z',[52]='0',[53]='1',[54]='2',[55]='3',[56]='4',[57]='5',[58]='6',[59]='7',[60]='8',[61]='9',[62]='-',[63]='_'}
+local base64bytes = {['-']="111110",['_']="111111",['0']="110100",['1']="110101",['2']="110110",['3']="110111",['4']="111000",['5']="111001",['6']="111010",['7']="111011",['8']="111100",['9']="111101",['A']="000000",['a']="011010",['B']="000001",['b']="011011",['C']="000010",['c']="011100",['D']="000011",['d']="011101",['E']="000100",['e']="011110",['F']="000101",['f']="011111",['G']="000110",['g']="100000",['H']="000111",['h']="100001",['I']="001000",['i']="100010",['J']="001001",['j']="100011",['K']="001010",['k']="100100",['L']="001011",['l']="100101",['M']="001100",['m']="100110",['N']="001101",['n']="100111",['O']="001110",['o']="101000",['P']="001111",['p']="101001",['Q']="010000",['q']="101010",['R']="010001",['r']="101011",['S']="010010",['s']="101100",['T']="010011",['t']="101101",['U']="010100",['u']="101110",['V']="010101",['v']="101111",['W']="010110",['w']="110000",['X']="010111",['x']="110001",['Y']="011000",['y']="110010",['Z']="011001",['z']="110011"}
+
+local haffman_t={[0]="00",[1]="01",[2]="100",[3]="101",[4]="1100",[5]="1101",[6]="11100",[7]="11101",[8]="111100",[9]="111101",[10]="1111100",[11]="1111101",[12]="11111100",[13]="11111101",[14]="111111100",[15]="111111101",}
+
+-- enc begin
+
+local function tobase64char(num)
+	while string.len(num)~=6 do
+		num=num.."1"
+	end
+	return base64chars[(tonumber(num,2))]
+end
+
+local function enc(data)
+
+	local count_t={[1]={n="0",p=0},[2]={n="1",p=0},[3]={n="2",p=0},[4]={n="3",p=0},[5]={n="4",p=0},[6]={n="5",p=0},[7]={n="6",p=0},[8]={n="7",p=0},[9]={n="8",p=0},[10]={n="9",p=0},[11]={n="a",p=0},[12]={n="b",p=0},[13]={n="c",p=0},[14]={n="d",p=0},[15]={n="e",p=0},[16]={n="f",p=0},}
+	local data_str={}
+
+	for i=1,string.len(data) do
+		n=string.byte(data,i)
+		a=n%16
+		b=(n-a)/16
+
+		count_t[a+1].p=count_t[a+1].p+1
+		count_t[b+1].p=count_t[b+1].p+1
+
+		data_str[#data_str+1]=b
+		data_str[#data_str+1]=a
+	end
+
+	table.sort(count_t,function(a,b) return a.p>b.p end)
+
+	local out_str={}
+	local this_haffman_t={}
+
+	for i,v in ipairs(count_t) do
+		this_haffman_t[tonumber(v.n,16)]=haffman_t[i-1]
+		out_str[#out_str+1]=v.n
+	end
+
+	for i,v in ipairs(data_str) do
+		data_str[i]=this_haffman_t[v]
+	end
+
+	local bin_str=table.concat(data_str)
+
+	for i=1,string.len(bin_str),6 do
+		local bin6=string.sub(bin_str,i,i+5)
+		out_str[#out_str+1]=tobase64char(bin6)
+	end
+
+	return table.concat(out_str)
+end
+
+-- enc end
+
+-- dec begin
+
+local function readabit(s)
+	local i=0
+	return function()
+		i=i+1
+		return string.sub(s,i,i) or ""
+	end
+end
+
+local function getxbit(r,h)
+	local t=""
+	while(r()=="1") do
+		t=t.."1"
+	end
+
+	t=t.."0"..r()
+	return h[t] or "X"
+end
+
+local function dec(data)
+
+	local this_haffman_t={}
+
+	for i=1,16 do
+		this_haffman_t[haffman_t[i-1]]=(string.sub(data,i,i))
+	end
+
+	local t={}
+	for i=17,string.len(data) do
+		t[#t+1]=base64bytes[(string.sub(data,i,i))]
+	end
+
+	local reader=readabit(table.concat(t))
+
+	local temp=getxbit(reader,this_haffman_t)
+
+	local out_str={}
+
+	while temp~="X" do
+		out_str[#out_str+1]=string.char(tonumber(temp..getxbit(reader,this_haffman_t),16))
+		temp=getxbit(reader,this_haffman_t)
+	end
+
+	return table.concat(out_str)
+end
+
+
+HumBase64={
+	enc=enc,
+	dec=dec,
+}
+
